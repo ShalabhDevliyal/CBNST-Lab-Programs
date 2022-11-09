327f2a37772c4a3caf3af0016a73b6b45a7ff1b4
ShalabhDevliyal <76624340+ShalabhDevliyal@users.noreply.github.com>
2022-11-09 05:44:15 +0530

    Create Falsi_short.c

---
+++ Falsi_short.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+//Equation: 3*x - cos x - 1
+
+double func(double x)
+{
+    return 3*x - cos(x) - 1;
+}
+int main()
+{
+    double a=0,b=0,x;
+    printf("Enter the starting value and the ending value: ");
+    scanf("%lf%lf",&a,&b);
+    int i = 0;
+    x = (a+b)/2;
+    while(fabs(func(x))>0.00001)
+    {
+        x = (b*func(a)-a*func(b))/(func(a)-func(b));
+        if(func(x)>0 && func(b)>0 || func(x)<0 && func(b)<0)
+        {
+            b = x;
+        }
+        else if(func(x)>0 && func(a)>0 || func(x)<0 && func(a)<0)
+        {
+            a = x;
+        }
+        printf("Iteration %d answer f(%lf): %lf\n",++i,x,func(x));
+    }
+    printf("Final Answer : %lf",x);
+    return 0;
+}

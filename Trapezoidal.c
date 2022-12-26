46e7dbc72b34168b9950c6f78dd32d39a186e594
ShalabhDevliyal <76624340+ShalabhDevliyal@users.noreply.github.com>
2022-12-26 17:35:20 +0530

    Update Trapezoidal.c

--- Trapezoidal.c
+++ Trapezoidal.c
@@ -7,10 +7,9 @@ float fun(float x)
 
 void solve(float h,float *y,int n)
 {
-	int i;
     float ans=y[0]+y[n];
     float sum=0;
-    for(i=1;i<n;i++)
+    for(int i=1;i<n;i++)
     {
         sum+=y[i];
     }
@@ -20,20 +19,23 @@ void solve(float h,float *y,int n)
 }
 int main()
 {
-	int i;
     float x0,xn;
     scanf("%f%f",&x0,&xn);
     
-    float h=1;
-    int n=(xn-x0)/h;
+    float h;
+    scanf("%f",&h);
+    int n=ceil((xn-x0)/h);
+    printf("n   =   %d\n",n);
     
     float y[n+1];
-    for(i=0;i<=n;i++)
+
+    printf("x            y\n");
+    for(int i=0;i<=n;i++)
     {
         y[i]=fun(x0);
         printf("%0.5f      %0.5f\n",x0,y[i]);
         x0+=h;
     }
 
-   solve(h,y,n);
+    solve(h,y,n);
 }
